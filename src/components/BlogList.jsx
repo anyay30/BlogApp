fbdbcd1eb796d64a44beb811780c539595242f1a
anyay30 <riya.2005yadav@gmail.com>
2026-06-22 00:13:25 +0530

    update

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import {blogCategories} from '../assets/assets/assets'
-//import{motion} from "motion/react"
+import { blogCategories, blog_data } from '../assets/assets/assets'
+import{motion} from "motion/react"
+import BlogCard from'./BlogCard'
 
 const BlogList = () => {
     const [menu, setMenu] = useState('All')
@@ -16,19 +17,22 @@ const BlogList = () => {
                className={`cursor-pointer text-gray-500 ${menu === item ? 'text-white px-4 pt-0.5' : ''}`}
              >
                {item}
-            {/* {menu===item&&(
-                <motion.div layoutId ='underline' transition={{type:'spring'}} className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></motion.div>
-            )} */}
+            {menu===item&&(
+                <motion.div layoutId ='underline' transition={{type:'spring',stiffness:500,damping:30}} className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></motion.div>
+            )}
 
                <div className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></div>
              </button>
            </div>
          ))}
         </div>
-        <div>
-            {
-
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
+            
+     {blog_data.filter((blog) => menu === "All" ? true : blog.category === menu).map((blog) => 
+                <BlogCard key={blog._id} blog={blog} />
+              )
             }
+           
         </div>
     </div>
   )
